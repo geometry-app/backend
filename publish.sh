602916ea43d409b09969599e9d523dc1cb9c7e75
Folleach <andrey@folleach.net>
2024-09-07 21:33:20 +0500

    fix version stamp

--- publish.sh
+++ publish.sh
@@ -6,10 +6,9 @@ tag=$3
 
 rm -rf out/*
 
-git rev-parse HEAD > out/version.txt
-
 cat Dockerfile | sed -r "s/AppEntryPoint/$app/" > TemporaryDockerfile
 dotnet publish -c Release $app/$app.csproj -o out
+git rev-parse HEAD > out/version.txt
 docker build -t $image -f TemporaryDockerfile --no-cache .
 docker tag $image $GEOMETRYAPP_DEPLOYMENT_REGISTRY/geometryapp/$image:$tag
 docker push $GEOMETRYAPP_DEPLOYMENT_REGISTRY/geometryapp/$image:$tag
